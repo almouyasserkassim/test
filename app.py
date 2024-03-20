11b1faff89ade6c9ecef5225e7ee87ba8ca63d34
almouyasserkassim <157636620+almouyasserkassim@users.noreply.github.com>
2024-03-20 16:36:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.header("Population totale dans les communes de Seine-Saint-Denis en 2021")
 
-conn_db_dirfi = st.connection("postgresql")
+conn_db_dirfi = st.connection("postgresql", type="sql")
 
 
 pop_totale = conn_db_dirfi.query("SELECT * FROM communes_93.population_totale_2021"
