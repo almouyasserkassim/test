4a1205c4199113032823d523302e2a41c308c2db
almouyasserkassim <157636620+almouyasserkassim@users.noreply.github.com>
2024-03-20 12:13:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-con
+st.header("Population totale dans les communes de Seine-Saint-Denis en 2021")
+
+conn = st.connection("sql")
+
+pop_totale = conn.query("SELECT * FROM communes_93.population_totale_2021"
+                       )
+
+st.dataframe(pop_totale)
