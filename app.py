92d036ee0ef7d905f9472b886ded4314a4249706
almouyasserkassim <157636620+almouyasserkassim@users.noreply.github.com>
2024-03-20 12:31:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,20 @@ import streamlit as st
 
 st.header("Population totale dans les communes de Seine-Saint-Denis en 2021")
 
-conn = st.connection("sql")
+@st.cache_resource
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
+    )
+
+conn = init_connection()
 
 pop_totale = conn.query("SELECT * FROM communes_93.population_totale_2021"
                        )
